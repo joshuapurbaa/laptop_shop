822ade07bb48db7f595594910391e126ba9cdbec
joshuapurbaa <yoshuapurba06@gmail.com>
2022-10-25 07:10:57 +0700

    Add testing skenario

--- test/provider/cart_manager_test.dart
+++ test/provider/cart_manager_test.dart
@@ -1,9 +1,15 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:laptop_shop/models/cart_manager.dart';
 
 void main() {
   test('A new item should be added', () {
     // Arrange
+    final cartManager = CartManager();
+    const mockNewCartItem = 'Mock Laptop Asus';
     // Act
+    cartManager.addToCart(mockNewCartItem);
     // Assert
+    final result = cartManager.cartList.contains(mockNewCartItem);
+    expect(result, true);
   });
 }
